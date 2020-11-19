707c43cd8ddbdf4bd669f25882a0a3f003cbc647
saranrajshri <shrisaranraj2001@gmail.com>
2020-11-19 14:44:28 +0530

    CSED

---
+++ CSES/permutations.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if(n == 1) {
+    	cout << 1;
+    	return 0;
+    }
+
+    if(n < 4) {
+    	cout << "NO SOLUTION";
+    	return 0;
+    }
+
+    if(n == 4) {
+    	cout << "3 1 4 2";
+    }else{
+    	for(int i = 1; i <= n; i += 2) {
+    		cout << i << " ";
+    	}	
+
+    	for(int i = 2; i <= n; i += 2) {
+    		cout << i << " ";
+    	}
+    }
+
+	return 0;	
+}
