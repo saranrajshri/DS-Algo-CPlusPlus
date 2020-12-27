4655b0f546968255061f92af0d3f97c4bcb1e3fe
saranrajshri <shrisaranraj2001@gmail.com>
2020-12-28 01:40:29 +0530

    Striver

---
+++ Striver/Binary Search Trees/4.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+
+struct TreeNode{
+	int val;
+	struct TreeNode *left, *right;
+};
+
+// A utility function to create a new BST node
+TreeNode* createNewNode(int val) {
+	TreeNode* newNode = (struct TreeNode *)malloc(sizeof(struct TreeNode));;
+	newNode->val = val;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+// A utility function to do inorder traversal of BST
+void inorder(TreeNode* root) {
+	if (root != NULL)
+	{
+		inorder(root->left);
+		std::cout << root->val << " ";
+		inorder(root->right);
+	}
+}
+
+/* A utility function to insert a new node with given key in BST */
+TreeNode* insert(TreeNode* root, int val) {
+
+	if (root == NULL) return createNewNode(val);
+
+	if (val < root->val) {
+		root->left = insert(root->left, val);
+	}
+	else if (val > root->val){
+		root->right = insert(root->right, val);
+	}
+
+	return root;
+}
+
+// Search a value in BST
+TreeNode* search(TreeNode* root, int valToBeFound) {
+	if(root == NULL || root->val == valToBeFound) return root;
+	
+	if(valToBeFound < root->val) {
+		return search(root->left, valToBeFound);
+	}
+	
+	return search(root->right, valToBeFound);
+}
+
+TreeNode* constructBST() {
+	
+	TreeNode *root = NULL;
+	root = insert(root, 50);
+	insert(root, 30);
+	insert(root, 20);
+	insert(root, 40);
+	insert(root, 70);
+	insert(root, 60);
+	insert(root, 80);
+
+	return root;
+}
+
+bool helper(TreeNode* root, long low, long high) {
+	if(root == NULL) return NULL;
+
+	if(root->val <= low || root->val >= high) return false;
+
+	return helper(root->left, low, root->val) && helper(root->right, root->val, high);
+}
+
+bool solve(TreeNode* root) {
+	return helper(root, LONG_MIN, LONG_MAX);	
+}
+
+int main(){	
+	TreeNode* root = constructBST();
+	if(solve(root)) {
+		std::cout << "Yes" << std::endl;
+	}else{
+		std::cout << "No" << std::endl;
+	}
+	return 0;
+}
---
+++ Striver/Binary Search Trees/5.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+
+struct TreeNode{
+	int val;
+	struct TreeNode *left, *right;
+};
+
+// A utility function to create a new BST node
+TreeNode* createNewNode(int val) {
+	TreeNode* newNode = (struct TreeNode *)malloc(sizeof(struct TreeNode));;
+	newNode->val = val;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+// A utility function to do inorder traversal of BST
+void inorder(TreeNode* root) {
+	if (root != NULL)
+	{
+		inorder(root->left);
+		std::cout << root->val << " ";
+		inorder(root->right);
+	}
+}
+
+/* A utility function to insert a new node with given key in BST */
+TreeNode* insert(TreeNode* root, int val) {
+
+	if (root == NULL) return createNewNode(val);
+
+	if (val < root->val) {
+		root->left = insert(root->left, val);
+	}
+	else if (val > root->val){
+		root->right = insert(root->right, val);
+	}
+
+	return root;
+}
+
+// Search a value in BST
+TreeNode* search(TreeNode* root, int valToBeFound) {
+	if(root == NULL || root->val == valToBeFound) return root;
+	
+	if(valToBeFound < root->val) {
+		return search(root->left, valToBeFound);
+	}
+	
+	return search(root->right, valToBeFound);
+}
+
+TreeNode* constructBST() {
+	
+	TreeNode *root = NULL;
+	root = insert(root, 50);
+	insert(root, 30);
+	insert(root, 20);
+	insert(root, 40);
+	insert(root, 70);
+	insert(root, 60);
+	insert(root, 80);
+
+	return root;
+}
+
+TreeNode* findPred(TreeNode* root, int val) {
+	if(root == NULL) return NULL;
+	TreeNode* currentNode = search(root, val);
+
+	if(currentNode->left) {
+		currentNode = currentNode->left;
+
+		while(currentNode->right) {
+			currentNode = currentNode->right;
+		}
+	}
+	return currentNode;
+}
+
+TreeNode* findSucc(TreeNode* root, int val) {
+	if(root == NULL) return NULL;
+	TreeNode* currentNode = search(root, val);
+	
+	if(currentNode->right) {
+		currentNode = currentNode->right;
+
+		while(currentNode->left) {
+			currentNode = currentNode->left;
+		}
+	}
+	return currentNode;
+}
+
+int main(){	
+
+	TreeNode* root = constructBST();
+	TreeNode* predNode = findPred(root, 70);
+	TreeNode* succNode = findSucc(root, 70);
+
+	std::cout << predNode->val << std::endl;
+	std::cout << succNode->val << std::endl;
+
+	return 0;
+}
